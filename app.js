8a136845b977fb871ccc10de53603501b480e93a
grvgb <budhorigaurav@gmail.com>
2019-04-17 04:29:09 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 let today= new Date();
 let formatdate= today.toDateString();
+console.log("hello this is gaurav budhori")
 let selectdate=document.getElementById('date');
 selectdate.innerHTML= formatdate
